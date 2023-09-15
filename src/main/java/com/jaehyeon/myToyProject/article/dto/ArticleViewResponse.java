ebf5b0898329c2331d391a96c5e7a87256131ba7
fanta4715 <fanta4715@naver.com>
2023-09-15 20:09:48 +0900

    article api 주석추가

--- src/main/java/com/jaehyeon/myToyProject/article/dto/ArticleViewResponse.java
+++ src/main/java/com/jaehyeon/myToyProject/article/dto/ArticleViewResponse.java
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
+//수정인지 아닌지를 파악하기 위해 필요한 DTO
 public class ArticleViewResponse {
     private Long id;
     private String title;
     private String content;
     private LocalDateTime createdAt;
 
+    //
     public ArticleViewResponse(Article article){
         this.id=article.getId();
         this.title = article.getTitle();
